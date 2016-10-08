f445e746dae6502e1401c8d6d1046054e4485d99
aadennis <dennis.circ@gmail.com>
2016-10-08 22:57:04 +0100

    Refactoring to extract function for getting single record

--- ConvertTo-CsvFromFixedWidth.ps1
+++ ConvertTo-CsvFromFixedWidth.ps1
@@ -1,5 +1,26 @@
 <# 
 .Synopsis 
+    Given a single passed record, used the spec detail to work out the csv equivalent
+    record to return.
+.Description
+    This is intended to be a private function
+.Example 
+    Get-CsvRecord -columnCount $noOfColumns -record $record -spec $spec
+#>
+function Get-CsvRecord($columnCount, $record, $spec) {
+     $outputRecord = [string]::Empty
+     1..$columnCount | % { 
+        # new column starts...
+        $currentColumn = $_  -1
+        $currentColDef = $spec.FriendsSpec.Fields[$currentColumn]
+        $outputRecord += $($record.substring($currentColDef.pos, $currentColDef.length)).trim() + ","
+
+     }
+     return $outputRecord
+}
+
+<# 
+.Synopsis 
     Given a fixed width data file, and a json spec containing the formatting rules,
     output a CSV version of the file. 
     It is assumed right now that the data starts at line 3
@@ -7,7 +28,7 @@
     ConvertTo-CsvFromFixedWidth -fixedWidthSpecFile ".\RealFriendsSpec.json" -fixedWidthDataFile ".\FWFile001.txt" -Verbose
 #>
 function ConvertTo-CsvFromFixedWidth {
-	[CmdletBinding(SupportsShouldProcess)]
+	[CmdletBinding()]
 	Param (
     [string][Parameter(mandatory)]
 	$fixedWidthSpecFile,
@@ -24,14 +45,7 @@ function ConvertTo-CsvFromFixedWidth {
         Get-Content $fixedWidthDataFile  | select -Skip 2 | foreach {
             # new record starts...
             $record = $_
-            $outputRecord = [string]::Empty
-            1..$noOfColumns | % { 
-                # new column starts...
-                $currentColumn = $_  -1
-                $currentColDef = $spec.FriendsSpec.Fields[$currentColumn]
-                $outputRecord += $($record.substring($currentColDef.pos, $currentColDef.length)).trim() + ","
-            }
-            $outputRecords += $outputRecord
+            $outputRecords += Get-CsvRecord -columnCount $noOfColumns -record $record -spec $spec
         }
         $outputRecords.Count
 	}
